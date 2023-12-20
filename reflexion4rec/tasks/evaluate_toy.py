07dc9ab42fd2618e98939166645dd8adb173ee9c
wzf2000 <nbjcwzf@163.com>
2023-12-20 10:44:19 +0800

    Update evaluate test task.

---
+++ reflexion4rec/tasks/evaluate_toy.py
@@ -0,0 +1,64 @@
+import json
+import openai
+import pandas as pd
+from loguru import logger
+from argparse import ArgumentParser
+from .base import Task
+from ..llms import AnyOpenAILLM
+from ..prompts import read_template
+from ..agents import ReactAgent
+
+class ToyEvaluateTask(Task):
+    @staticmethod
+    def parse_task_args(parser: ArgumentParser) -> ArgumentParser:
+        parser.add_argument('--api_config', type=str, default='config/api-config.json', help='Api configuration file')
+        parser.add_argument('--toy_data', type=str, required=True, help='Toy data file')
+        parser.add_argument('--agent', type=str, default='react', choices=['react'], help='Agent name')
+        parser.add_argument('--task', type=str, default='rp', choices=['rp'], help='Task name')
+        return parser
+    
+    def run(self, api_config: str, toy_data: str, agent: str, task: str):
+        with open(api_config, 'r') as f:
+            api_config = json.load(f)
+        openai.api_base = api_config['api_base']
+
+        react_llm = AnyOpenAILLM(
+            temperature=api_config['temperature'],
+            max_tokens=api_config['max_tokens'],
+            model_name=api_config['model'],
+            model_kwargs={"stop": "\n"},
+            openai_api_key=api_config['api_key'],
+        )
+        
+        prompt = read_template(f"config/prompts/{agent}_prompt.json")[f'test_{agent}_prompt']
+        df = pd.read_csv(toy_data)
+        df['history'] = df['history'].apply(lambda x: '\n'.join(x.split('\n')[-20:]))
+        
+        data_prompt = read_template(f"config/prompts/{task}.json")[f"{task}_data_prompt"]
+        test_data = data_prompt.format(
+            user_id=df['user_id'][0],
+            user_profile=df['user_profile'][0],
+            history=df['history'][0],
+            target_item_id=df['item_id'][0],
+            target_item_attributes=df['target_item_attributes'][0]
+        )
+        logger.info(f"Test data: {test_data}")
+        
+        # test one step
+
+        if agent == 'react':
+            agent_model = ReactAgent(
+                agent_prompt=prompt,
+                react_examples="",
+                actor_llm=react_llm
+            )
+
+            agent_model.set_data(input=test_data, context="", gt_answer=str(df['rating'][0]))
+
+            logger.info(f'Init: {agent_model._build_agent_prompt()}')
+            agent_model.step()
+        else:
+            raise NotImplementedError
+        
+if __name__ == '__main__':
+    ToyEvaluateTask().launch()
