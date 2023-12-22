1220bf9336cf653dcad034cd2420e4889f23dcd3
wzf2000 <nbjcwzf@163.com>
2023-12-22 20:17:30 +0800

    Add evaluate task.

--- reflexion4rec/tasks/__init__.py
+++ reflexion4rec/tasks/__init__.py
@@ -1,4 +1,5 @@
 # Description: all tasks are defined here
 from .evaluate_toy import ToyEvaluateTask
+from .evaluate import EvaluateTask
 from .preprocess import PreprocessTask
 from .rlhf import RLHFTrainingTask
