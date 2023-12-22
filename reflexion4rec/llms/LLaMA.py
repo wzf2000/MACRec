8d829f131eb369933cac08f7a1457b8a57b04c3a
wzf2000 <nbjcwzf@163.com>
2023-12-22 22:07:39 +0800

    Update LLaMA config.

--- reflexion4rec/llms/LLaMA.py
+++ reflexion4rec/llms/LLaMA.py
@@ -4,7 +4,7 @@ from .basellm import BaseLLM
 
 class LLaMA(BaseLLM):
     def __init__(self, *args, **kwargs):
-        model_path = kwargs.get('model_path', '"meta-llama/Llama-2-7b-chat-hf"')
+        model_path = kwargs.get('model_path', 'meta-llama/Llama-2-7b-hf')
         self.pipe = pipeline("text-generation", model=model_path)
     
     def __call__(self, prompt: str):
