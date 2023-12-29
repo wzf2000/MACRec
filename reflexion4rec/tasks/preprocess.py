7302f058fdb9598cd2dac692fe520cdc945e2ec3
wzf2000 <nbjcwzf@163.com>
2023-12-29 14:55:30 +0800

    Add amazon dataset in preprocess task.

--- reflexion4rec/tasks/preprocess.py
+++ reflexion4rec/tasks/preprocess.py
@@ -1,17 +1,19 @@
 from argparse import ArgumentParser
 from .base import Task
-from ..dataset import ml100k_process_data
+from ..dataset import ml100k_process_data, amazon_process_data
 
 class PreprocessTask(Task):
     @staticmethod
     def parse_task_args(parser: ArgumentParser) -> ArgumentParser:
         parser.add_argument('--data_dir', type=str, required=True, help='input file')
-        parser.add_argument('--dataset', type=str, required=True, choices=['ml-100k'], help='output file')
+        parser.add_argument('--dataset', type=str, required=True, choices=['ml-100k', 'amazon'], help='output file')
         return parser
     
     def run(self, data_dir: str, dataset: dir):
         if dataset == 'ml-100k':
             ml100k_process_data(data_dir)
+        elif dataset == 'amazon':
+            amazon_process_data(data_dir)
         else:
             raise NotImplementedError
         
