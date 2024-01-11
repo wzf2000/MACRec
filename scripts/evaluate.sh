fefb0c7d680e68069dc84c3ee0854a2774a11a12
wzf2000 <nbjcwzf@163.com>
2024-01-11 14:36:38 +0800

    Update evaluation shell script.

--- scripts/evaluate.sh
+++ scripts/evaluate.sh
@@ -4,12 +4,26 @@
 
 ## Evaluate ReAct
 ### rating prediction task
-python main.py --main Evaluate --test_data data/ml-100k/toy-5.csv --agent react --task rp --json_mode
+python main.py --main Evaluate --test_data data/ml-100k/test.csv --agent react --task rp --json_mode
 ### sequential recommendation task
-python main.py --main Evaluate --test_data data/ml-100k/toy-5.csv --agent react --task sr --json_mode
+python main.py --main Evaluate --test_data data/ml-100k/test.csv --agent react --task sr --json_mode
 
 ## Evaluate Reflexion
 ### rating prediction task
-python main.py --main Evaluate --test_data data/ml-100k/toy-5.csv --agent react_reflect --task rp --json_mode
+python main.py --main Evaluate --test_data data/ml-100k/test.csv --agent react_reflect --task rp --json_mode
 ### sequential recommendation task
-python main.py --main Evaluate --test_data data/ml-100k/toy-5.csv --agent react_reflect --task sr --json_mode
+python main.py --main Evaluate --test_data data/ml-100k/test.csv --agent react_reflect --task sr --json_mode
+
+# Evaluate on Amazon-Beauty (1000 samples)
+
+## Evaluate ReAct
+### rating prediction task
+python main.py --main Evaluate --test_data data/Beauty/test_sample1000.csv --agent react --task rp --json_mode
+### sequential recommendation task
+python main.py --main Evaluate --test_data data/Beauty/test_sample1000.csv --agent react --task sr --json_mode
+
+## Evaluate Reflexion
+### rating prediction task
+python main.py --main Evaluate --test_data data/Beauty/test_sample1000.csv --agent react_reflect --task rp --json_mode
+### sequential recommendation task
+python main.py --main Evaluate --test_data data/Beauty/test_sample1000.csv --agent react_reflect --task sr --json_mode
