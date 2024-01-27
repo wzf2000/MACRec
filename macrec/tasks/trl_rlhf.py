9e314ec3d4d93fc5fd40ccd434c390cb5d7d8d79
yuyq18 <yuyq0114@gmail.com>
2024-01-27 16:33:16 +0800

    Optimize training files

--- macrec/tasks/trl_rlhf.py
+++ macrec/tasks/trl_rlhf.py
@@ -1,4 +1,7 @@
+import os
+import time
 import json
+from loguru import logger
 from argparse import ArgumentParser
 from tqdm import tqdm
 
@@ -18,16 +21,25 @@ class RLHFTrainingTask(Task):
     @staticmethod
     def parse_task_args(parser: ArgumentParser):
         parser.add_argument('--config_path', type=str, required=True, help='Path to the config file')
+        parser.add_argument('--model_path', type=str, help='Path to the model')
+        parser.add_argument('--data_file', type=str, help='Path to the data file')
+        parser.add_argument('--epochs', type=int, help='Number of epochs to train')
         return parser
     
     def train(self, epochs: int = 1):
+        base_dir = os.path.join('ckpts/', str(int(time.time())))
+        os.makedirs(base_dir, exist_ok=True)
         for epoch in range(epochs):
             for batch_id, batch in tqdm(enumerate(self.trainer.dataloader)):
                 query_tensors, response_tensors = batch['input_ids'], batch['output_ids']
                 rewards = batch['rewards']
-
+                logger.debug(f"query_tensors: {query_tensors}")
+                logger.debug(f"query_tensors length: {len(query_tensors[0])}")
+                logger.debug(f"response_tensors: {response_tensors}")
+                logger.debug(f"response_tensors length: {len(response_tensors[0])}")
                 stats = self.trainer.step(query_tensors, response_tensors, rewards)
                 self.trainer.log_stats(stats, batch, rewards, columns_to_log=["query", "response"])
+            self.trainer.save_pretrained(os.path.join(base_dir, f'epoch-{epoch}'))
     
     def get_jsonl_dataset(self, path: str):
         prompts, responses, rewards = [], [], []
@@ -39,14 +51,29 @@ class RLHFTrainingTask(Task):
                 rewards.append(item['reward'])
         return OfflinePPODataset(prompts, responses, rewards, self.tokenizer)
 
-    def run(self, config_path: str):
+    def run(self, config_path: str, model_path: str, data_file: str, epochs: int, *args, **kwargs):
         with open(config_path, 'r') as config_f:
             config = json.load(config_f)
 
-        model_path = config.get('model_path', 'lmsys/vicuna-7b-v1.5-16k')
+        if model_path is None:
+            model_path = config.get('model_path', 'lmsys/vicuna-7b-v1.5-16k')
+        self.model_path = model_path
         self.tokenizer = AutoTokenizer.from_pretrained(model_path)
+        
+        if epochs is None:
+            epochs = config.get('epochs', 1)
 
-        data_kwargs = config.get('data_kwargs', {})
+        if data_file is not None:
+            if data_file.endswith('.jsonl'):
+                data_kwargs = {
+                    'type': 'jsonl',
+                    'path': data_file
+                }
+            else:
+                raise NotImplementedError
+        else:
+            data_kwargs = config.get('data_kwargs', {})
+        
         data_type = data_kwargs.get('type', 'jsonl')
         if data_type == 'jsonl':
             path = data_kwargs.get('path', None)
@@ -62,9 +89,8 @@ class RLHFTrainingTask(Task):
 
         device_map = {"": Accelerator().local_process_index}
         # TODO: add more Model support
-        self.model = AutoModelForCausalLMWithValueHead.from_pretrained(model_path, device_map=device_map, peft_config=peft_config)
-        self.model = self.model.to(torch.float16)
+        self.model = AutoModelForCausalLMWithValueHead.from_pretrained(model_path, device_map=device_map, peft_config=peft_config, torch_dtype=torch.bfloat16)
 
         self.trainer = PPOTrainer(ppo_config, self.model, tokenizer=self.tokenizer, dataset=dataset, data_collator=collator)
         
-        self.train()
+        self.train(epochs=epochs)
