29d8adfd687ba1f428fa4932b254242583e825b0
wzf2000 <nbjcwzf@163.com>
2024-01-31 15:03:06 +0800

    Update new tasks for modules.

--- macrec/tasks/__init__.py
+++ macrec/tasks/__init__.py
@@ -2,9 +2,11 @@
 from .evaluate import EvaluateTask
 from .preprocess import PreprocessTask
 from .sample import SampleTask
-# from .rlhf import RLHFTrainingTask
 from .trl_rlhf import RLHFTrainingTask
 from .feedback import FeedbackTask
 from .test import TestTask
 from .reward_update import RewardUpdateTask
 from .calculate import CalculateTask
+from .mac_evaluate import MACEvaluateTask
+from .mac_test import MACTestTask
+from .mac_feedback import MACFeedbackTask
