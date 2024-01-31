50fbe27f7e62215fbb5351a0c915703effcaeefd
wzf2000 <nbjcwzf@163.com>
2024-01-31 15:01:22 +0800

    Add json mode support for OpenAI llms.

--- macrec/llms/openai.py
+++ macrec/llms/openai.py
@@ -3,9 +3,10 @@ from langchain.schema import HumanMessage
 from .basellm import BaseLLM
 
 class AnyOpenAILLM(BaseLLM):
-    def __init__(self, model_name: str = 'gpt-3.5-turbo', *args, **kwargs):
+    def __init__(self, model_name: str = 'gpt-3.5-turbo', json_mode: bool = False, *args, **kwargs):
         # Determine model type from the kwargs
         self.model_name = model_name
+        self.json_mode = json_mode
         self.max_tokens: int = kwargs.get('max_tokens', 256)
         self.max_context_length: int = 16384 if '16k' in model_name else 32768 if '32k' in model_name else 4096
         if model_name.split('-')[0] == 'text':
@@ -16,6 +17,7 @@ class AnyOpenAILLM(BaseLLM):
             self.model_type = 'chat'
     
     def __call__(self, prompt: str, json_mode: bool = False, *args, **kwargs):
+        json_mode = json_mode or self.json_mode
         if json_mode and self.model_type != 'chat':
             raise ValueError("json_mode is only available for chat models")
         if json_mode and self.model_name not in ['gpt-3.5-turbo-1106', 'gpt-4-1106-preview']:
@@ -34,7 +36,7 @@ class AnyOpenAILLM(BaseLLM):
             del self.model.model_kwargs['response_format']
             return content
         if self.model_type == 'completion':
-            return self.model.invoke(prompt)
+            return self.model.invoke(prompt).content
         else:
             return self.model.invoke(
                 [
